725b069be2808c98282e0167b9c5cadf6ee06de0
Guilherme Ledo <guilhermeledochagas@gmail.com>
2026-01-24 19:57:17 -0300

    Adding compute and operations logic.

--- js/index.js
+++ js/index.js
@@ -37,6 +37,45 @@ class Calculator {
         }
     }
 
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            case '%':
+                computation = prev % current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
     updateDisplay() {
         this.currentOperandElement.innerText = this.currentOperand;
         this.previousOperandElement.innerText = this.previousOperand;
