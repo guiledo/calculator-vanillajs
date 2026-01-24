27b7067de2e8eed75de1fb2f6cf0ac3258c98536
Guilherme Ledo <guilhermeledochagas@gmail.com>
2026-01-24 20:13:07 -0300

    Added some safeguards in case the user press unexpected keys.

--- js/index.js
+++ js/index.js
@@ -131,7 +131,13 @@ deleteButton.addEventListener('click', () => {
 
 document.addEventListener('keydown', (e) => {
     let key = e.key;
-    
+    const allowedKeys = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 
+        '+', '-', '*', '/', 'Enter', 'Backspace', 'Escape', '%'
+    ];
+
+    if (!allowedKeys.includes(key)) return;
+
     const button = document.querySelector(`button[data-key="${key}"]`);
     
     if (button) {
